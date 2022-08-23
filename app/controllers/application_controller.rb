3e78b81b4510b68239bc346068574fd78d7e4a54
hariharnath <hariharnath_maheswaran@softprodigy.com>
2022-08-23 18:55:17 +0530

    rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # rescue_from Exception, :with => :error_generic
+  rescue_from Exception, :with => :error_generic
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
